5e248f554f62109076e17d103af67d288240b802
EnhaoSun <enhao.sun@outlook.com>
2022-10-15 23:41:49 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Introduction
+A simple static page in React
+# Run Project
+```shell
+npm run dev
+```
+# Screenshot
+![](./screen-shot.png)
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='container'>
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
